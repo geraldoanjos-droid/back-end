3b07788400a5a332beadfe53eae12500dbe5470d
geraldoanjos-droid <geraldo.anjos@escola.pr.gov.br>
2026-03-19 09:00:36 -0300

    Create ex012.py

---
+++ exercicios/ex012.py
@@ -0,0 +1,5 @@
+largura = float(input("digite a largura da parede:"))
+altura = float (input(digite a altura da parede:))
+area = altura*largura
+tinta  = area/2
+print("a parede com a{altura}x{largura} tem uma area de {area}, e sera utilizada {tinta},")
