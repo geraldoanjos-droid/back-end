ee3d4336ae5fc49165b1c41ff2765262e063bbf0
geraldoanjos-droid <geraldo.anjos@escola.pr.gov.br>
2026-03-19 08:52:40 -0300

    Create ex011.py

---
+++ exercicios/ex011.py
@@ -0,0 +1,3 @@
+dinhero = float(input("digite a quantia em dinhero:"))
+dolar = dinhero/5.24
+print(f"esta quantia de dinhero em reais:{:.3f} e equivalente a {:3f} em dolares ". format (dinhero, dolar))
