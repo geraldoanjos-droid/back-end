3ff2d7e1fa01da347ebe20875c2f784805eac62c
geraldoanjos-droid <geraldo.anjos@escola.pr.gov.br>
2026-08-20 10:41:47 -0300

    Add script to sum even numbers from user input

---
+++ exercicios/ex049.py
@@ -0,0 +1,8 @@
+soma =
+cont =
+for c in range (1,7):
+num int(input(f"digite o {c) valor "))
+if num%20:
+soma += num
+cont cont +1
+print (f"Você informou (cont) numeros pares soma dos numeros pares é (soma}") a
