6de3ea35c7bb69aa7f91311497000e9cbb6d3963
geraldoanjos-droid <geraldo.anjos@escola.pr.gov.br>
2026-08-20 10:42:52 -0300

    Create ex051.py

---
+++ exercicios/ex051.py
@@ -0,0 +1,22 @@
+1 from datetime import date
+2 maior = 0
+3
+menor = 0
+4
+for c in range(1,8):
+5
+anoint (input(f"Em que ano a (c) pessoa nasceu?"))
+6
+idade date.today().year ano
+7
+if idade >= 18:
+B
+maior += 1
+9
+else:
+10
+menor += 1
+11
+print (f"Ao todo tivemos (maior) pessoas maiores de idade")
+12
+print (f"E também tivemos (menor pessoas maiores de idade")
