805e45ff3b03d7aef843e3fbd9610801a0dc8d61
geraldoanjos-droid <geraldo.anjos@escola.pr.gov.br>
2026-08-20 10:43:39 -0300

    Add exercicios/ex053.py for age and gender analysis

---
+++ exercicios/ex053.py
@@ -0,0 +1,20 @@
+media e
+maior
+cont
+for c in range(1,5):
+print("*5+ f"(c) pessoa"+"-"*5)
+nome str(input("None")).strip()
+idade int(input("Idade"))
+sexo = str(input("Sexo[m/f]")).strip()
+media (media + idade)/4
+if c = - 1 and sexo in "Mm":
+maior idade
+senhor nome
+elif idade > maior and sexom":
+maior idade
+senhor nome
+11 sexo- "f" and idade < 20:
+cont += 1
+print("A média de idade do grupo é de (media)")
+print(f"o homem mais velho tem maior anos e se chama senhor}")
+print("Ao todo são (cont) mulheres con menos de 20 anos")
