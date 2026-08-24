1833f95119a2dbafb5e4fdd9fc31eaa78943432e
geraldoanjos-droid <geraldo.anjos@escola.pr.gov.br>
2026-08-24 10:40:13 -0300

    Create ex056.py

---
+++ exercicios/ex056.py
@@ -0,0 +1,33 @@
+
+from time import sleep
+n1 = int(input("Qual é o primeiro número?"))
+n2 = int(input("Qual é o segundo número?"))
+opcao 0
+while opcao! 5:
+print('[1] somar')
+print('[2] multiplicar')
+print('[3] maior')
+print('[4] novos números')
+print('[5] sair do programa")
+opcao = int(input('Qual é a sua opcão?'))
+if opcao == 1:
+soma n1 + n2
+print('A soma de (n1) + (n2) é igual a soma')
+elif opcao == 2:
+multiplicacaon1 n2
+print(f'A multiplicação de (nl) e (n2) é igual a (multiplicacao)')
+
+elif opcao 3:
+if ni > n2: print("0 (nl) é maior que (12)")
+olif n * 2 > n1z
+(n2) é maior que (n1)")
+else:
+print("Ambos os números são iguals")
+elif opcao 4:
+print('informe os números novamente)
+ni int(input("Qual é o primeiro número?")) n2= int(input("Qual é o segundo número?"))
+elif opcao 5:
+print('Finalizando...')
+else: print('opção inválida. Tente novamente')
+sleep(2)
+print('Acabou')
